b3b5ed8c9fd75a437aa097919f6254d3dea8d146
Sai Kumar <60894063+sai2729@users.noreply.github.com>
2024-12-05 13:41:01 -0600

    Update WebConfig.java

--- src/main/java/com/bakery/BakeryManagement/config/WebConfig.java
+++ src/main/java/com/bakery/BakeryManagement/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // Match API paths
-                        .allowedOrigins("http://localhost:4200") // Angular app's origin
+                        .allowedOrigins("https://bakerywebsite-aab57.web.app/") // Angular app's origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
